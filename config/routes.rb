6c3ddff588d0e3a7f279090e1f683831a0783a33
Web Artist <nomi.sheikh@gmail.com>
2016-11-23 17:02:29 +0400

    Added create action, and added rspec tests for new and create actions.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pins#index'
   get "pins/name-:slug" => "pins#show_by_name", as: 'pin_by_name'  
-  
+
   resources :pins
 
   get "/library" => "pins#index"
+
+  get "/pins/new" => "pins#new"
 
   
     
